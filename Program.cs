e912656e83fee7da973090eed41fc94efd79bb62
HENRIQUE MORAES ONORATO <henrique.onorato@cs.up.edu.br>
2025-03-24 20:18:57 -0300

    comit inicial

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Digite seu nome completo");
+
+string nome = Console.ReadLine();
+Console.WriteLine($"Olá, {nome}!");
+
+Console.WriteLine("Qual seu ano de nascimento?");
+int anoDeNascimento = int.Parse(Console.ReadLine());
+DateTime dataAtual = DateTime.Now;
+int idade = dataAtual.Year - anoDeNascimento;
+Console.WriteLine($"Você tem {idade} anos.");
+Console.WriteLine("Seu codigo unicode é: ");
+foreach (char letra in nome)
+{
+    Console.Write((int)letra + " ");
+}

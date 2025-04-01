c5ff9e6641ebc5ed0d13171ad1b527b8a0de3672
HENRIQUE MORAES ONORATO <henrique.onorato@cs.up.edu.br>
2025-03-31 21:18:29 -0300

    wip

--- obj/Debug/net8.0/projeto.henriqueonorato.24.AssemblyInfo.cs
+++ obj/Debug/net8.0/projeto.henriqueonorato.24.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projeto.henriqueonorato.24")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e912656e83fee7da973090eed41fc94efd79bb62")]
 [assembly: System.Reflection.AssemblyProductAttribute("projeto.henriqueonorato.24")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projeto.henriqueonorato.24")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
